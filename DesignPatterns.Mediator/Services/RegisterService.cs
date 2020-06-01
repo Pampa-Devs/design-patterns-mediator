9bcbb1d422c555bfb251f3103b87bf17f94002e1
felipe-allmeida <felipe.allmeida.dev@gmail.com>
2020-06-01 01:03:26 -0300

    Hello World

---
+++ DesignPatterns.Mediator/Services/RegisterService.cs
@@ -0,0 +1,48 @@
+﻿using DesignPatterns.Mediator.Data;
+using DesignPatterns.Mediator.Models;
+using DesignPatterns.Mediator.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DesignPatterns.Mediator.Services
+{
+    public class RegisterService
+    {
+        private readonly NotificationHandler _notificationHandler;
+        private readonly MockedProductRepository _repository;
+
+        public RegisterService(NotificationHandler notificationHandler, MockedProductRepository repository)
+        {
+            _notificationHandler = notificationHandler;
+            _repository = repository;
+        }
+
+        public bool RegisterProduct(string name, string description, decimal price)
+        {
+            var product = new Product(name, description, price);
+
+            if (!product.IsValid())
+            {
+                foreach(var error in product.GetErrors())
+                {
+                    _notificationHandler.PublishNotification(error);
+                }
+                return false;
+            }
+
+            return Commit(product);
+        }
+
+        private bool Commit(Product product)
+        {
+            if (!_repository.AddProduct(product))
+            {
+                _notificationHandler.PublishNotification("Commit failed");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ DesignPatterns.Mediator/Services/RentService.cs
@@ -0,0 +1,33 @@
+﻿using DesignPatterns.Mediator.Data;
+using DesignPatterns.Mediator.Models;
+using DesignPatterns.Mediator.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Mediator.Services
+{
+    public class RentService
+    {
+        private readonly NotificationHandler _notificationHandler;
+        private readonly MockedProductRepository _repository;
+
+        public RentService(NotificationHandler notificationHandler, MockedProductRepository repository)
+        {
+            _notificationHandler = notificationHandler;
+            _repository = repository;
+        }
+
+        public Product RentProduct(string name)
+        {
+            var product = _repository.GetProduct(name);
+
+            if (product == null)
+            {
+                _notificationHandler.PublishNotification("Product is not available");
+            }
+
+            return product;
+        }
+    }
+}
